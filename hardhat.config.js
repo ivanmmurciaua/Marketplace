1e390013a07c0ed5f3c1364a8261dab9959eacb5
Iván M.M <32136734+ivanmmurciaua@users.noreply.github.com>
2022-08-30 15:48:19 +0200

    All tests passing

--- hardhat.config.js
+++ hardhat.config.js
@@ -41,20 +41,6 @@ module.exports = {
     showTimeSpent : true,
     coinmarketcap : process.env.GAS_KEY
   },
-  networks: {
-    testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      gasPrice: 20000000000,
-      accounts: [process.env.PRIVATE_KEY]
-    },
-    mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   }
